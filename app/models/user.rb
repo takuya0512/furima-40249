1802cd967d1a39671e0b6a9e737b6920d1b01ae2
takuya0512 <ta98euleka@gmail.com>
2024-01-11 19:43:16 +0900

    バリデーション整え

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
-
+  validates :nickname, :last_name, :first_name, :birthday, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\z/ }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
+  validates :password_confirmation, presence: true, if: :password
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/ }
 end
