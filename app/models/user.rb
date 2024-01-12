bd04ba19116917cc757474dfc101bd3b9a59860d
takuya0512 <ta98euleka@gmail.com>
2024-01-12 18:15:00 +0900

    カラム名修正、インデント整え

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])/ }
+  validates :nickname,              presence: true
+  validates :email,                 presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :password,              presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])/ }
   validates :password_confirmation, presence: true
-  validates :last_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
-  validates :birthdate, presence: true
+  validates :last_name,             presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
+  validates :first_name,            presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
+  validates :last_name_kana,        presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :first_name_kana,       presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :birthdate,             presence: true
 end
