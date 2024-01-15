b7e2f98e5b065d8b6beb518e4b9582088b369f13
takuya0512 <ta98euleka@gmail.com>
2024-01-15 11:13:37 +0900

    モデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_11_020128) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_15_020412) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.text "description", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_fee_covered_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "delivery_period_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_11_020128) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
