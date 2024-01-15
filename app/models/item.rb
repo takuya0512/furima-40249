f3eef3b582f0cb68d2a1fcf9fb95170a8c108613
takuya0512 <ta98euleka@gmail.com>
2024-01-15 11:56:15 +0900

    ActiveStorage、ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_covered
+  belongs_to :prefecture
+  belongs_to :delivery_period
 end
