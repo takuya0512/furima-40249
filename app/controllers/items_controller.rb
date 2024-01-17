5d720a007c12bb7534564f04492dd41451984e03
takuya0512 <ta98euleka@gmail.com>
2024-01-17 14:17:38 +0900

    エラーハンドリングの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
   end
@@ -9,17 +10,17 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-
+    
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_covered_id, :prefecture_id, :delivery_period_id, :image)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_covered_id, :prefecture_id, :delivery_period_id, :image).merge(user_id: current_user.id)
   end 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -21,10 +21,10 @@ class Item < ApplicationRecord
     validates :shipping_fee_covered_id
     validates :prefecture_id
     validates :delivery_period_id
-    validates :price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
-  with_options numericality: { other_than: 0 } do
+  with_options numericality: { other_than: 0, message: 'can\'t be blank' }, presence: true do
     validates :category_id
     validates :condition_id
     validates :prefecture_id
