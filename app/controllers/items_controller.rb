76f564625f22e5c40f77a66c436525dfbc6f1d20
takuya0512 <ta98euleka@gmail.com>
2024-01-29 10:25:17 +0900

    アクション定義統一

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user_id && @item.order.nil?
+    if current_user.id != @item.user_id || @item.order.present?
       redirect_to root_path
     end
   end
