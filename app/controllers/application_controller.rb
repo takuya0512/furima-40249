e91715c191d1462838e87ae7a64be5adcde2efa2
takuya0512 <ta98euleka@gmail.com>
2024-01-29 16:28:52 +0900

    BASIC認証抜けてたので追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,5 +8,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [:nickname, :birthdate, :first_name, :last_name, :first_name_kana, :last_name_kana]
     )
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '1234'
+    end
   end
 end
